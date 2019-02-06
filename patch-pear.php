30f6cf35b9752a4051ea823fcb11456cc5d194a3
Dmitry Balabka <dmitry.balabka@gmail.com>
2019-02-06 12:01:54 +0200

    Added patched version of PEAR installer

---
+++ patch-pear.php
@@ -0,0 +1,2 @@
+<?php
+copy('https://raw.githubusercontent.com/pear/Console_Getopt/417f9c464eb3922997177e8857e17d020a3e4618/Console/Getopt.php', 'phar://install-pear-nozlib.phar/Console/Getopt.php');
